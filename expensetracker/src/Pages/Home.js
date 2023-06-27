d982c28e0f48c0eb63ff16945f0ec3e72a2aadd2
vikasvikky12345 <rebbavikas2000@gmail.com>
2023-06-28 00:03:58 +0530

    Logout User

--- expensetracker/src/Pages/Home.js
+++ expensetracker/src/Pages/Home.js
@@ -1,16 +1,16 @@
-import React,{useState,useEffect} from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import ProfileUpdate from './ProfileUpdate';
-import SendVerificationEmail from './SendVerificationMail';
-
+import { getAuth, signOut, sendEmailVerification } from 'firebase/auth';
+import './Home.css';
 
 const Home = () => {
   const [isProfileComplete, setProfileComplete] = useState(false);
+  const [isEmailVerified, setEmailVerified] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // Check if the user's profile is complete (replace this with your logic)
     const checkProfileCompletion = () => {
-      // Assume that the user's profile is complete if the 'displayName' property exists
       const isComplete = !!localStorage.getItem('displayName');
       setProfileComplete(isComplete);
     };
@@ -18,22 +18,70 @@ const Home = () => {
     checkProfileCompletion();
   }, []);
 
+  useEffect(() => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (user && user.emailVerified) {
+      setEmailVerified(true);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        localStorage.removeItem('token');
+        navigate('/login');
+      })
+      .catch((error) => {
+        console.log('Logout error:', error);
+      });
+  };
+
+  const handleSendVerification = () => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    sendEmailVerification(user)
+      .then(() => {
+        alert('Verification email sent. Please check your email.');
+      })
+      .catch((error) => {
+        console.log('Send verification error:', error);
+      });
+  };
+
   return (
-    <div>
-      {isProfileComplete ? (
-        <>
-          <h1>Welcome to Expense Tracker</h1>
-          <SendVerificationEmail />
-        </>
-      ) : (
-        <>
-          <h2>Your profile is incomplete</h2>
-          <p>Please complete your profile to continue.</p>
-          <Link to="/profileupdate">
-            <button>Complete Profile</button>
-          </Link>
-        </>
-      )}
+    <div className="home-container">
+      <div className="home-header">
+        <h1 className="welcome-heading">Welcome to Expense Tracker</h1>
+        {isProfileComplete && isEmailVerified && (
+          <button className="logout-button" onClick={handleLogout}>
+            Logout
+          </button>
+        )}
+      </div>
+      <div className="home-content">
+        {!isProfileComplete ? (
+          <>
+            <h2>Your profile is incomplete</h2>
+            <p>Please complete your profile to continue.</p>
+            <Link to="/profileupdate">
+              <button>Complete Profile</button>
+            </Link>
+          </>
+        ) : (
+          <>
+            {!isEmailVerified && (
+              <>
+                <p>Your email is not verified.</p>
+                <button onClick={handleSendVerification}>Send Verification Email</button>
+              </>
+            )}
+          </>
+        )}
+      </div>
       {!isProfileComplete && <ProfileUpdate setProfileComplete={setProfileComplete} />}
     </div>
   );
