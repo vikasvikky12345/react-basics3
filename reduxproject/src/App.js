3e3666a47a0e1bcd5b5c68ea326903acb4c99cb1
vikasvikky12345 <rebbavikas2000@gmail.com>
2023-07-03 20:19:25 +0530

    Auth Reducer

--- reduxproject/src/App.js
+++ reduxproject/src/App.js
@@ -1,9 +1,22 @@
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+
 import Counter from './Components/Counter';
+import Header from './Components/Header';
+import Auth from './Components/Auth';
+import UserProfile from './Components/UserProfile';
 
 
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
+    </Fragment>
   );
 }
 
