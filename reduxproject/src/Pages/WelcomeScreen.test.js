e2f2760bdbbd3e668a5267347131fc21db094011
vikasvikky12345 <rebbavikas2000@gmail.com>
2023-07-13 11:40:54 +0530

    testing2

--- reduxproject/src/Pages/WelcomeScreen.test.js
+++ reduxproject/src/Pages/WelcomeScreen.test.js
@@ -3,6 +3,24 @@ import WelcomeScreen from './WelcomeScreen';
 
 test('renders Hello World as a text', () => {
   render(<WelcomeScreen />);
-  const helloWorldElement = screen.getByText('Welcome to the exense tracker', {exact:flase});
+  const helloWorldElement = screen.getByText('Welcome to the expense tracker!');
   expect(helloWorldElement).toBeInTheDocument();
+});
+
+test('renders the "Update User" link', () => {
+  render(<WelcomeScreen />);
+  const updateUserLink = screen.getByRole('link', { name: 'Update User' });
+  expect(updateUserLink).toBeInTheDocument();
+});
+
+test('renders the "Logout" button', () => {
+  render(<WelcomeScreen />);
+  const logoutButton = screen.getByRole('button', { name: 'Logout' });
+  expect(logoutButton).toBeInTheDocument();
+});
+
+test('renders the ExpenseForm component', () => {
+  render(<WelcomeScreen />);
+  const expenseForm = screen.getByTestId('expense-form');
+  expect(expenseForm).toBeInTheDocument();
 });
