d49d0b646164f346c6b78b80b4818680c0c479c4
vikasvikky12345 <rebbavikas2000@gmail.com>
2023-07-18 19:20:47 +0530

    Prerendering the powers of Next JS

--- mettingnext/pages/index.js
+++ mettingnext/pages/index.js
@@ -4,21 +4,31 @@ const DUMMY_MEETUPS = [
   {
     id: 'm1',
     title: 'A First Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
     address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!'
+    description: 'This is a first meetup!',
   },
   {
     id: 'm2',
     title: 'A Second Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
     address: 'Some address 10, 12345 Some City',
-    description: 'This is a second meetup!'
-  }
+    description: 'This is a second meetup!',
+  },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS
+    },
+    revalidate: 1
+  }; 
 }
 
 export default HomePage;
