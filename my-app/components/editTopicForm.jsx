5a3c2e2bdc69be821d04c237201a0e5e3d57335c
vikasvikky12345 <rebbavikas2000@gmail.com>
2023-07-19 16:11:42 +0530

    Time to save the TODOs

--- my-app/components/editTopicForm.jsx
+++ my-app/components/editTopicForm.jsx
@@ -1,5 +1,9 @@
 export default function editTopicForm(){
     return(
-        <div>editTopicForm</div>
+        <form className="flex flex-col gap-3">
+            <input className="border border-slate-500 px-8 py-2" type="text" placeholder="topic title"/>
+            <input className="border border-slate-500 px-8 py-2" type="text" placeholder="topic description"/>
+            <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">update Task</button>
+        </form>
     )
 }
